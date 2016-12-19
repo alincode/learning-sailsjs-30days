85e1494269d39585dbf2f4bae2c4de1a75e434b9
艾霖 劉 <ailinliu@ailindeMacBook-Pro.local>
2016-12-19 11:37:08 +0800

    sails generate controller article create update destroy

---
+++ example/ex06/generate-controller-example/api/controllers/ArticleController.js
@@ -0,0 +1,41 @@
+/**
+ * ArticleController
+ *
+ * @description :: Server-side logic for managing articles
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `ArticleController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `ArticleController.update()`
+   */
+  update: function (req, res) {
+    return res.json({
+      todo: 'update() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `ArticleController.destroy()`
+   */
+  destroy: function (req, res) {
+    return res.json({
+      todo: 'destroy() is not implemented yet!'
+    });
+  }
+};
+
